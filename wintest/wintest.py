367768bd957de1d68f65a74d323e6fbabc3d5238
Jennifer Walker <jenfly@gmail.com>
2016-07-27 17:17:21 -0700

    Added wintest script

---
+++ wintest/wintest.py
@@ -0,0 +1,16 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('Hello world!')
+print(1 + 2)
+
+for i in range(5):
+    print(i)
+
+
+x = np.arange(10)
+y = x + 2
+plt.figure()
+plt.plot(x, y)
+plt.show()
